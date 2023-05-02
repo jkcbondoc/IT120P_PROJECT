2ecab63eb5dc637adb3e49eaba56fa03ae5f6e50
joshbondoc <95848841+joshbondoc@users.noreply.github.com>
2023-05-02 16:59:51 +0800

    Add files via upload

---
+++ ProductsServices.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width-device-width, initial scale =1.0">
+    <title>Water Luv | Products and Services</title>
+    <link rel="stylesheet" href="ProductsServicesStyle.css")>
+</head>
+
+<body>
+    <header>
+    <image align="left" src="logo.png" alt="waterluv logo" width="95px" height="95px" border="0">
+    <nav class="navigation">
+        <a href="About-Us.php">About Us</a>
+        <a href="ProductsServices.php">Products and Services</a>
+        <a href="Orders.php">Orders</a>
+        <a href="register.php">Login</a>
+    </nav>
+    </header>
+
+    <section>
+  	<br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="a">
+    <p align="center"><font face="courier new"><b><u><font color="Black"><font size="6">Products</a></font></font></font></p></b></u>
+    <br />
+    <center>
+        <h2>Alkaline Water</h2>
+    <img width="200" height="180" src="alkaline.png">
+    <p><b><i>Description:</b></i> This type of water has a higher pH level than tap water, which some people believe</p>
+        <br />
+        <h2>Mineral Water</h2>
+    <img width="200" height="180" src="mineral.png">
+    <p><b><i>Description: </b></i> This type of water comes from a natural spring or well and contains  </p>
+    <p>naturally occurring minerals like calcium, magnesium, and potassium. </p>
+       <br />
+        <h2>Purified Water</h2>
+    <img width="200" height="180" src="purified.png">
+    <p><b><i>Description:</b></i> This type of water has gone through a filtration process to remove impurities,</p>
+    <p> such as chemicals and bacteria, to make it safe for drinking.</p>
+    </center>
+    	<br />
+    	</div>
+      <center>
+    <div class="a">
+    <p align="center"><font face="courier new"><b><u><font color="Black"><font size="6">Services</a></font></font></font></p></b></u>
+    	<br />
+        <h2>Water Refilling</h2>
+    <img width="200" height="180" src="refil.png">
+        <p><b><i>Description:</b></i> We offer water refilling services for customers who want to refill </p>
+    	<p>their water bottles with our high-quality water.</p>
+        <br />
+        <h2>Delivery Services</h2>
+    <img width="200" height="180" src="delivery.png">
+        <p><b><i>Description:</b></i> We offer delivery services for customers who want to receive</p>
+    	<p>our water directly to their doorstep.</p>
+        <br />
+        <h2>Water Analysis</h2>
+    <img width="200" height="180" src="analysis.png">
+        <p><b><i>Description:</b></i> We offer water analysis services to help customers determine the quality </p>
+    	<p>of their water and identify any potential contaminants.</p>
+    	</center>
+    	<br />
+  </section>
+
+    <script src="script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
